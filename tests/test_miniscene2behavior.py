6f66453232e86aaf3edc1d8513328a512ae7fb16
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-11 21:00:54 -0400

    Add miniscene2behavior test

---
+++ tests/test_miniscene2behavior.py
@@ -0,0 +1,33 @@
+# TODO: get rid of if False when model is public
+import unittest
+import requests
+import zipfile
+from time import sleep
+import sys
+from kabr_tools import miniscene2behavior
+
+
+class TestMiniscene2Behavior(unittest.TestCase):
+    def test_annotate(self):
+        if False:
+            # download model from huggingface
+            url = "https://huggingface.co/imageomics/" \
+                + "x3d-kabr-kinetics/resolve/main/" \
+                + "checkpoint_epoch_00075.pyth.zip"
+            r = requests.get(url, allow_redirects=True, timeout=300)
+            with open("checkpoint_epoch_00075.pyth.zip", "wb") as f:
+                f.write(r.content)
+
+            # unzip model checkpoint
+            with zipfile.ZipFile("checkpoint_epoch_00075.pyth.zip", "r") as zip_ref:
+                zip_ref.extractall(".")
+
+            # wait for tracks_extractor test
+            sleep(5)
+
+            # unzip model checkpoint
+            sys.argv = ["miniscene2behavior.py",
+                        "--checkpoint", "checkpoint_epoch_00075.pyth",
+                        "--miniscene", "mini-scenes/tests|examples|DJI_0068",
+                        "--video", "DJI_0068",]
+            miniscene2behavior.main()
--- tests/test_tracks_extractor.py
+++ tests/test_tracks_extractor.py
@@ -4,7 +4,7 @@ from kabr_tools import tracks_extractor
 
 
 class TestTracksExtractor(unittest.TestCase):
-    def test_parse_args(self):
+    def test_extractor(self):
         sys.argv = ["tracks_extractor.py",
                     "--video", "tests/examples/DJI_0068.mp4",
                     "--annotation", "tests/examples/tracks.xml"]
