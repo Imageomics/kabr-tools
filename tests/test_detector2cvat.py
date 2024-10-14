6aa96f2d1ed2ab839ee79254fad63f5c24d3406c
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-14 16:43:27 -0400

    Update path

--- tests/test_detector2cvat.py
+++ tests/test_detector2cvat.py
@@ -6,6 +6,6 @@ from kabr_tools import detector2cvat
 class TestDetector2Cvat(unittest.TestCase):
     def test_detector(self):
         sys.argv = ["detector2cvat.py",
-                    "--video", "detection_example/examples",
-                    "--save", "detection_example/output"]
+                    "--video", "tests/detection_example",
+                    "--save", "tests/detection_example/output"]
         detector2cvat.main()
