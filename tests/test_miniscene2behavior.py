a5212185a8013dc80eca9ad351b0d54bca348e85
John Bradley <johnbradley2008@gmail.com>
2024-10-15 14:52:33 -0400

    Add example mock/patch test

--- tests/test_miniscene2behavior.py
+++ tests/test_miniscene2behavior.py
@@ -1,10 +1,18 @@
 # TODO: get rid of if False when model is public
 import unittest
+from unittest.mock import Mock, patch
+import os
 import requests
 import zipfile
 from time import sleep
 import sys
 from kabr_tools import miniscene2behavior
+from kabr_tools.miniscene2behavior import annotate_miniscene
+import torch
+import pandas as pd
+
+TESTSDIR = os.path.dirname(os.path.realpath(__file__))
+EXAMPLESDIR = os.path.join(TESTSDIR, "examples")
 
 
 class TestMiniscene2Behavior(unittest.TestCase):
@@ -34,3 +42,33 @@ class TestMiniscene2Behavior(unittest.TestCase):
                         "--miniscene", "mini-scenes/tests|examples|DJI_0068",
                         "--video", "DJI_0068",]
             miniscene2behavior.main()
+
+    # Replace process_cv2_inputs function with a mock
+    @patch('kabr_tools.miniscene2behavior.process_cv2_inputs')
+    def test_annotate_miniscene(self, process_cv2_inputs):
+
+        # Create fake model that always returns a prediction of 1
+        mock_model = Mock()
+        mock_model.return_value = torch.tensor([1])
+
+        # Create fake cfg
+        mock_config = Mock(
+            DATA=Mock(NUM_FRAMES=16,
+                    SAMPLING_RATE=5,
+                    TEST_CROP_SIZE=300),
+            NUM_GPUS=0,
+            OUTPUT_DIR=''
+        )
+
+        output_csv = '/tmp/annotation_data.csv'
+
+        annotate_miniscene(cfg=mock_config,
+                           model=mock_model,
+                           miniscene_path=os.path.join(EXAMPLESDIR, "MINISCENE1"),
+                           video='DJI_0068',
+                           output_path=output_csv)
+
+        # Read in output CSV and make sure we have the expected columns and at least one row
+        df = pd.read_csv(output_csv, sep=' ')
+        self.assertEqual(list(df.columns), ["video","track","frame", "label"])
+        self.assertGreater(len(df.index), 0)
