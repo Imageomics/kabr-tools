06d01eee81ccfd737022503eab3bf50ca6ab5419
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-17 21:20:10 -0400

    Patch detector2cvat

--- tests/test_detector2cvat.py
+++ tests/test_detector2cvat.py
@@ -1,11 +1,13 @@
-# import unittest
-# import sys
-# from kabr_tools import detector2cvat
+import unittest
+import sys
+from unittest.mock import patch
+from kabr_tools import detector2cvat
 
 
-# class TestDetector2Cvat(unittest.TestCase):
-#     def test_detector(self):
-#         sys.argv = ["detector2cvat.py",
-#                     "--video", "tests/detection_example",
-#                     "--save", "tests/detection_example/output"]
-#         detector2cvat.main()
+class TestDetector2Cvat(unittest.TestCase):
+    @patch('kabr_tools.miniscene2behavior.cv2.imshow')
+    def test_detector(self, imshow):
+        sys.argv = ["detector2cvat.py",
+                    "--video", "tests/detection_example",
+                    "--save", "tests/detection_example/output"]
+        detector2cvat.main()
