a25f8936050842967c948343c19ed42bd5a77699
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-17 22:48:14 -0400

    Mock getTrackbarPos

--- tests/test_player.py
+++ tests/test_player.py
@@ -9,7 +9,11 @@ class TestPlayer(unittest.TestCase):
     @patch('kabr_tools.player.cv2.namedWindow')
     @patch('kabr_tools.player.cv2.createTrackbar')
     @patch('kabr_tools.player.cv2.setTrackbarPos')
-    def test_run(self, imshow, namedWindow, createTrackbar, setTrackbarPos):
+    @patch('kabr_tools.player.cv2.getTrackbarPos')
+    def test_run(self, imshow, namedWindow, createTrackbar, setTrackbarPos, getTrackbarPos):
+        # mock getTrackbarPos
+        getTrackbarPos.return_value = 0
+
         # run player
         sys.argv = ["player.py",
                     "--folder", "tests/behavior_example/DJI_0001",
