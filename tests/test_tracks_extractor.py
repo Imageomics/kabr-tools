869b17b85a3444783035c4ce54cb8c8907945255
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-17 22:45:09 -0400

    Test tracks extractor parser

--- tests/test_tracks_extractor.py
+++ tests/test_tracks_extractor.py
@@ -10,3 +10,29 @@ class TestTracksExtractor(unittest.TestCase):
                     "--video", "tests/detection_example/DJI_0068.mp4",
                     "--annotation", "tests/detection_example/DJI_0068.xml"]
         tracks_extractor.main()
+
+    def test_parse_arg_min(self):
+        # parse arguments
+        sys.argv = ["tracks_extractor.py",
+                    "--video", "tests/detection_example/DJI_0068.mp4",
+                    "--annotation", "tests/detection_example/DJI_0068.xml"]
+        args = tracks_extractor.parse_args()
+        self.assertEqual(args.video, "tests/detection_example/DJI_0068.mp4")
+        self.assertEqual(
+            args.annotation, "tests/detection_example/DJI_0068.xml")
+        self.assertEqual(args.tracking, False)
+        self.assertEqual(args.imshow, False)
+
+    def test_parse_arg_full(self):
+        # parse arguments
+        sys.argv = ["tracks_extractor.py",
+                    "--video", "tests/detection_example/DJI_0068.mp4",
+                    "--annotation", "tests/detection_example/DJI_0068.xml",
+                    "--tracking",
+                    "--imshow"]
+        args = tracks_extractor.parse_args()
+        self.assertEqual(args.video, "tests/detection_example/DJI_0068.mp4")
+        self.assertEqual(
+            args.annotation, "tests/detection_example/DJI_0068.xml")
+        self.assertEqual(args.tracking, True)
+        self.assertEqual(args.imshow, True)
