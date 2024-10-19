8b4c2c7149467a0b300cf4e748f5223ecfc0ac21
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-18 20:39:16 -0400

    Test cvat2slowfast parser

--- tests/test_cvat2slowfast.py
+++ tests/test_cvat2slowfast.py
@@ -10,3 +10,28 @@ class TestCvat2Slowfast(unittest.TestCase):
                     "--dataset", "tests/slowfast",
                     "--classes", "ethogram/classes.json"]
         cvat2slowfast.main()
+
+    def test_parse_arg_min(self):
+        # parse arguments
+        sys.argv = ["cvat2slowfast.py",
+                    "--miniscene", "tests/behavior_example",
+                    "--dataset", "tests/slowfast",
+                    "--classes", "ethogram/classes.json"]
+        args = cvat2slowfast.parse_args()
+        self.assertEqual(args.miniscene, "tests/behavior_example")
+        self.assertEqual(args.dataset, "tests/slowfast")
+        self.assertEqual(args.classes, "ethogram/classes.json")
+        self.assertEqual(args.old2new, None)
+
+    def test_parse_arg_full(self):
+        # parse arguments
+        sys.argv = ["cvat2slowfast.py",
+                    "--miniscene", "tests/behavior_example",
+                    "--dataset", "tests/slowfast",
+                    "--classes", "ethogram/classes.json",
+                    "--old2new", "ethogram/old2new.json"]
+        args = cvat2slowfast.parse_args()
+        self.assertEqual(args.miniscene, "tests/behavior_example")
+        self.assertEqual(args.dataset, "tests/slowfast")
+        self.assertEqual(args.classes, "ethogram/classes.json")
+        self.assertEqual(args.old2new, "ethogram/old2new.json")
