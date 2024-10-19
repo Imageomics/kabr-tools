8308c60c9006e68c3491e00e38f7eebf9b1da646
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-18 20:45:53 -0400

    Test cvat2ultralytics parsing

--- tests/test_cvat2ultralytics.py
+++ tests/test_cvat2ultralytics.py
@@ -10,3 +10,31 @@ class TestCvat2Ultralytics(unittest.TestCase):
                     "--annotation", "tests/detection_example",
                     "--dataset", "tests/ultralytics"]
         cvat2ultralytics.main()
+
+    def test_parse_arg_min(self):
+        # parse arguments
+        sys.argv = ["cvat2ultralytics.py",
+                    "--video", "tests/detection_example",
+                    "--annotation", "tests/detection_example",
+                    "--dataset", "tests/ultralytics"]
+        args = cvat2ultralytics.parse_args()
+        self.assertEqual(args.video, "tests/detection_example")
+        self.assertEqual(args.annotation, "tests/detection_example")
+        self.assertEqual(args.dataset, "tests/ultralytics")
+        self.assertEqual(args.skip, 10)
+        self.assertEqual(args.label2index, None)
+
+    def test_parse_arg_full(self):
+        # parse arguments
+        sys.argv = ["cvat2ultralytics.py",
+                    "--video", "tests/detection_example",
+                    "--annotation", "tests/detection_example",
+                    "--dataset", "tests/ultralytics",
+                    "--skip", "5",
+                    "--label2index", "ethogram/label2index.json"]
+        args = cvat2ultralytics.parse_args()
+        self.assertEqual(args.video, "tests/detection_example")
+        self.assertEqual(args.annotation, "tests/detection_example")
+        self.assertEqual(args.dataset, "tests/ultralytics")
+        self.assertEqual(args.skip, 5)
+        self.assertEqual(args.label2index, "ethogram/label2index.json")
