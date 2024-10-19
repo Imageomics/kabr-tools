c011e0e4a2d502b7271b4352f63591974ee663d7
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-18 20:53:29 -0400

    Test detector2cvat parsing

--- tests/test_detector2cvat.py
+++ tests/test_detector2cvat.py
@@ -11,3 +11,12 @@ class TestDetector2Cvat(unittest.TestCase):
                     "--video", "tests/detection_example",
                     "--save", "tests/detection_example/output"]
         detector2cvat.main()
+
+    def test_parse_arg_full(self):
+        # parse arguments
+        sys.argv = ["detector2cvat.py",
+                    "--video", "tests/detection_example",
+                    "--save", "tests/detection_example/output"]
+        args = detector2cvat.parse_args()
+        self.assertEqual(args.video, "tests/detection_example")
+        self.assertEqual(args.save, "tests/detection_example/output")
