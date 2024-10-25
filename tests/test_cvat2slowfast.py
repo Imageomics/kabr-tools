e76167227fd3ac46ba534c4f62c87440d880e9d2
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-25 15:02:49 -0400

    Update cvat2slowfast

--- tests/test_cvat2slowfast.py
+++ tests/test_cvat2slowfast.py
@@ -4,34 +4,47 @@ from kabr_tools import cvat2slowfast
 
 
 class TestCvat2Slowfast(unittest.TestCase):
+    def setUp(self):
+        self.tool = "cvat2slowfast.py"
+        self.miniscene = "tests/behavior_example"
+        self.dataset = "tests/slowfast"
+        self.classes = "ethogram/classes.json"
+        self.old2new = "ethogram/old2new.json"
+
     def test_run(self):
-        sys.argv = ["cvat2slowfast.py",
-                    "--miniscene", "tests/behavior_example",
-                    "--dataset", "tests/slowfast",
-                    "--classes", "ethogram/classes.json"]
+        sys.argv = [self.tool,
+                    "--miniscene", self.miniscene,
+                    "--dataset", self.dataset,
+                    "--classes", self.classes]
         cvat2slowfast.main()
 
     def test_parse_arg_min(self):
         # parse arguments
-        sys.argv = ["cvat2slowfast.py",
-                    "--miniscene", "tests/behavior_example",
-                    "--dataset", "tests/slowfast",
-                    "--classes", "ethogram/classes.json"]
+        sys.argv = [self.tool,
+                    "--miniscene", self.miniscene,
+                    "--dataset", self.dataset,
+                    "--classes", self.classes]
         args = cvat2slowfast.parse_args()
-        self.assertEqual(args.miniscene, "tests/behavior_example")
-        self.assertEqual(args.dataset, "tests/slowfast")
-        self.assertEqual(args.classes, "ethogram/classes.json")
+
+        # check parsed argument values
+        self.assertEqual(args.miniscene, self.miniscene)
+        self.assertEqual(args.dataset, self.dataset)
+        self.assertEqual(args.classes, self.classes)
+
+        # check default argument values
         self.assertEqual(args.old2new, None)
 
     def test_parse_arg_full(self):
         # parse arguments
         sys.argv = ["cvat2slowfast.py",
-                    "--miniscene", "tests/behavior_example",
-                    "--dataset", "tests/slowfast",
-                    "--classes", "ethogram/classes.json",
-                    "--old2new", "ethogram/old2new.json"]
+                    "--miniscene", self.miniscene,
+                    "--dataset", self.dataset,
+                    "--classes", self.classes,
+                    "--old2new", self.old2new]
         args = cvat2slowfast.parse_args()
-        self.assertEqual(args.miniscene, "tests/behavior_example")
-        self.assertEqual(args.dataset, "tests/slowfast")
-        self.assertEqual(args.classes, "ethogram/classes.json")
-        self.assertEqual(args.old2new, "ethogram/old2new.json")
+
+        # check parsed argument values
+        self.assertEqual(args.miniscene, self.miniscene)
+        self.assertEqual(args.dataset, self.dataset)
+        self.assertEqual(args.classes, self.classes)
+        self.assertEqual(args.old2new, self.old2new)
