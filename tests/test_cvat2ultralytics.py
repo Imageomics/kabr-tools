5a1478edbdc1e0880bfe443cb7a935c39e6efa2c
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-25 15:10:29 -0400

    Update cvat2ultralytics

--- tests/test_cvat2ultralytics.py
+++ tests/test_cvat2ultralytics.py
@@ -4,37 +4,51 @@ from kabr_tools import cvat2ultralytics
 
 
 class TestCvat2Ultralytics(unittest.TestCase):
+    def setUp(self):
+        self.tool = "cvat2ultralytics.py"
+        self.video = "tests/detection_example"
+        self.annotation = "tests/detection_example"
+        self.dataset = "tests/ultralytics"
+        self.skip = "5"
+        self.label2index = "ethogram/label2index.json"
+
     def test_run(self):
-        sys.argv = ["cvat2ultralytics.py",
-                    "--video", "tests/detection_example",
-                    "--annotation", "tests/detection_example",
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--annotation", self.annotation,
                     "--dataset", "tests/ultralytics"]
         cvat2ultralytics.main()
 
     def test_parse_arg_min(self):
         # parse arguments
-        sys.argv = ["cvat2ultralytics.py",
-                    "--video", "tests/detection_example",
-                    "--annotation", "tests/detection_example",
-                    "--dataset", "tests/ultralytics"]
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--annotation", self.annotation,
+                    "--dataset", self.dataset]
         args = cvat2ultralytics.parse_args()
-        self.assertEqual(args.video, "tests/detection_example")
-        self.assertEqual(args.annotation, "tests/detection_example")
-        self.assertEqual(args.dataset, "tests/ultralytics")
+
+        # check parsed argument values
+        self.assertEqual(args.video, self.video)
+        self.assertEqual(args.annotation, self.annotation)
+        self.assertEqual(args.dataset, self.dataset)
+
+        # check default argument values
         self.assertEqual(args.skip, 10)
         self.assertEqual(args.label2index, None)
 
     def test_parse_arg_full(self):
         # parse arguments
-        sys.argv = ["cvat2ultralytics.py",
-                    "--video", "tests/detection_example",
-                    "--annotation", "tests/detection_example",
-                    "--dataset", "tests/ultralytics",
-                    "--skip", "5",
-                    "--label2index", "ethogram/label2index.json"]
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--annotation", self.annotation,
+                    "--dataset", self.dataset,
+                    "--skip", self.skip,
+                    "--label2index", self.label2index]
         args = cvat2ultralytics.parse_args()
-        self.assertEqual(args.video, "tests/detection_example")
-        self.assertEqual(args.annotation, "tests/detection_example")
-        self.assertEqual(args.dataset, "tests/ultralytics")
-        self.assertEqual(args.skip, 5)
-        self.assertEqual(args.label2index, "ethogram/label2index.json")
+
+        # check parsed argument values
+        self.assertEqual(args.video, self.video)
+        self.assertEqual(args.annotation, self.annotation)
+        self.assertEqual(args.dataset, self.dataset)
+        self.assertEqual(args.skip, self.skip)
+        self.assertEqual(args.label2index, self.label2index)
