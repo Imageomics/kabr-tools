9cfcfc0816971b5a33f7ee0c48a409727aa273b2
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-10-25 15:56:06 -0400

    Update tracks_extractor

--- tests/test_tracks_extractor.py
+++ tests/test_tracks_extractor.py
@@ -4,35 +4,45 @@ from kabr_tools import tracks_extractor
 
 
 class TestTracksExtractor(unittest.TestCase):
+    def setUp(self):
+        self.tool = "tracks_extractor.py"
+        self.video = "tests/behavior_example/DJI_0001"
+        self.annotation = "tests/behavior_example/DJI_0001"
+
+
     def test_run(self):
         # run tracks_extractor
-        sys.argv = ["tracks_extractor.py",
-                    "--video", "tests/detection_example/DJI_0068.mp4",
-                    "--annotation", "tests/detection_example/DJI_0068.xml"]
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--annotation", self.annotation]
         tracks_extractor.main()
 
     def test_parse_arg_min(self):
         # parse arguments
-        sys.argv = ["tracks_extractor.py",
-                    "--video", "tests/detection_example/DJI_0068.mp4",
-                    "--annotation", "tests/detection_example/DJI_0068.xml"]
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--annotation", self.annotation]
         args = tracks_extractor.parse_args()
-        self.assertEqual(args.video, "tests/detection_example/DJI_0068.mp4")
-        self.assertEqual(
-            args.annotation, "tests/detection_example/DJI_0068.xml")
+
+        # check parsed arguments
+        self.assertEqual(args.video, self.video)
+        self.assertEqual(args.annotation,self.annotation)
+
+        # check default arguments
         self.assertEqual(args.tracking, False)
         self.assertEqual(args.imshow, False)
 
     def test_parse_arg_full(self):
         # parse arguments
-        sys.argv = ["tracks_extractor.py",
-                    "--video", "tests/detection_example/DJI_0068.mp4",
-                    "--annotation", "tests/detection_example/DJI_0068.xml",
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--annotation", self.annotation,
                     "--tracking",
                     "--imshow"]
         args = tracks_extractor.parse_args()
-        self.assertEqual(args.video, "tests/detection_example/DJI_0068.mp4")
-        self.assertEqual(
-            args.annotation, "tests/detection_example/DJI_0068.xml")
+
+        # check parsed arguments
+        self.assertEqual(args.video, self.video)
+        self.assertEqual(args.annotation, self.annotation)
         self.assertEqual(args.tracking, True)
         self.assertEqual(args.imshow, True)
