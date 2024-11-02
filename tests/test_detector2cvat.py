72eb862a7874ce2128ae1ae45b32a24e458a5fe7
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-11-02 12:04:23 -0400

    Update detector2cvat save directory

--- tests/test_detector2cvat.py
+++ tests/test_detector2cvat.py
@@ -1,27 +1,35 @@
 import unittest
 import sys
+import os
+import shutil
 from unittest.mock import MagicMock, patch
 from kabr_tools import detector2cvat
 
+SAVE_DIR = "tests/detector2cvat"
 
 class TestDetector2Cvat(unittest.TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        # Remove output files after all tests have been completed
+        if os.path.exists(SAVE_DIR):
+            shutil.rmtree(SAVE_DIR)
+
     def setUp(self):
         self.tool = "detector2cvat.py"
         self.video = "tests/detection_example"
-        self.save = "tests/detection_example/output"
 
-    # @patch('kabr_tools.detector2cvat.cv2.imshow')
-    # def test_run(self, imshow):
-    #     sys.argv = [self.tool,
-    #                 "--video", self.video,
-    #                 "--save", self.save]
-    #     detector2cvat.main()
+    @patch('kabr_tools.detector2cvat.cv2.imshow')
+    def test_run(self, imshow):
+        # Check if tool runs on real data
+        sys.argv = [self.tool,
+                    "--video", self.video,
+                    "--save", f"{SAVE_DIR}/run"]
+        detector2cvat.main()
 
 
     @patch('kabr_tools.detector2cvat.cv2.imshow')
     @patch('kabr_tools.detector2cvat.YOLOv8')
     def test_mock_yolo(self, yolo, imshow):
-
         # Create fake YOLO
         yolo_instance = MagicMock()
         yolo_instance.forward.return_value = [[[0, 0, 0, 0], 0.95, 0]]
@@ -29,18 +37,19 @@ class TestDetector2Cvat(unittest.TestCase):
         yolo.return_value = yolo_instance
 
         # Run detector2cvat
+        save = f"{SAVE_DIR}/mock/0"
         sys.argv = [self.tool,
                     "--video", self.video,
-                    "--save", self.save]
+                    "--save", save]
         detector2cvat.main()
 
     def test_parse_arg_full(self):
         # parse arguments
         sys.argv = [self.tool,
                     "--video", self.video,
-                    "--save", self.save]
+                    "--save", SAVE_DIR]
         args = detector2cvat.parse_args()
 
         # check parsed argument values
         self.assertEqual(args.video, self.video)
-        self.assertEqual(args.save, self.save)
+        self.assertEqual(args.save, SAVE_DIR)
