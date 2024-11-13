50feea9b043169cdbd2f63a7e1472fb9b756e3b6
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-11-13 17:05:59 -0500

    Delete tracks_extractor output

--- tests/test_tracks_extractor.py
+++ tests/test_tracks_extractor.py
@@ -1,21 +1,39 @@
 import unittest
 import sys
+import os
+import shutil
+from unittest.mock import patch
 from kabr_tools import tracks_extractor
+
+
+def del_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+
+def run():
+    tracks_extractor.main()
 
 
 class TestTracksExtractor(unittest.TestCase):
     def setUp(self):
         self.tool = "tracks_extractor.py"
-        self.video = "tests/behavior_example/DJI_0001"
-        self.annotation = "tests/behavior_example/DJI_0001"
+        self.video = "tests/detection_example/DJI_0068.mp4"
+        self.annotation = "tests/detection_example/DJI_0068.xml"
 
+        # Remove output directory before test
+        del_dir("mini-scenes")
+
+    def tearDown(self):
+        # Remove output directory after test
+        del_dir("mini-scenes")
 
     def test_run(self):
         # run tracks_extractor
         sys.argv = [self.tool,
                     "--video", self.video,
                     "--annotation", self.annotation]
-        tracks_extractor.main()
+        run()
 
     def test_parse_arg_min(self):
         # parse arguments
@@ -26,13 +44,17 @@ class TestTracksExtractor(unittest.TestCase):
 
         # check parsed arguments
         self.assertEqual(args.video, self.video)
-        self.assertEqual(args.annotation,self.annotation)
+        self.assertEqual(args.annotation, self.annotation)
 
         # check default arguments
         self.assertEqual(args.tracking, False)
         self.assertEqual(args.imshow, False)
 
-    def test_parse_arg_full(self):
+        # run tracks_extractor
+        run()
+
+    @patch('kabr_tools.tracks_extractor.cv2.imshow')
+    def test_parse_arg_full(self, imshow):
         # parse arguments
         sys.argv = [self.tool,
                     "--video", self.video,
@@ -46,3 +68,6 @@ class TestTracksExtractor(unittest.TestCase):
         self.assertEqual(args.annotation, self.annotation)
         self.assertEqual(args.tracking, True)
         self.assertEqual(args.imshow, True)
+
+        # run tracks_extractor
+        tracks_extractor.main()
