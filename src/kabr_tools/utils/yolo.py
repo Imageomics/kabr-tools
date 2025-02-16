5d1b0731097a91e9c04a02d54a0761557e463272
zhong-al <74470739+zhong-al@users.noreply.github.com>
2025-02-16 09:37:04 -0800

    DetectionModel

--- src/kabr_tools/utils/yolo.py
+++ src/kabr_tools/utils/yolo.py
@@ -1,9 +1,13 @@
 import numpy as np
-from ultralytics import YOLO
-from torch.nn.modules.container import Sequential
 import torch
 
+from torch.nn.modules.container import Sequential
+from ultralytics import YOLO
+from ultralytics.nn.tasks import DetectionModel
+
 torch.serialization.add_safe_globals([Sequential])
+torch.serialization.add_safe_globals([DetectionModel])
+
 
 class YOLOv8:
     def __init__(self, weights="yolov8x.pt", imgsz=640, conf=0.5):
